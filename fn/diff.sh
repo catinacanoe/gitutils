2f34c2ddc6dc3084f32af1513941028f0732cfff
catinacanoe <catinacanoe@proton.me>
2023-12-27 23:38:13 -0800

    fixed a dumb bug

--- fn/diff.sh
+++ fn/diff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function diff() {
-    [ -z "$arg" ] && git diff && return
+    [ -z "$1" ] && git diff && return
 
     arg="$1"
     shift
@@ -9,7 +9,7 @@ function diff() {
     if [ -n "$(echo "$arg" | grep '^[0-9]$')" ]; then
         git diff "HEAD~$arg" $@
     elif [ "$arg" == "c" ]; then
-	git diff --chached $@
+	git diff --cached $@
     else
         git diff $arg $@
     fi
