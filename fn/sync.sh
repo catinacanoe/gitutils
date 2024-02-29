2f906680d55e48f9cee1323c74483de62179edf0
catinacanoe <catinacanoe@proton.me>
2024-02-29 08:10:37 -0800

    fixed typo

--- fn/sync.sh
+++ fn/sync.sh
@@ -4,8 +4,8 @@ function sync() {
     stat="$(status | tail -n 1)"
 
     case "$stat" in
-        *"behind"*) echo "crypt is behind, pulling" && git pull ;;
-        *"ahead"*) echo "crypt is ahead, pushing" && diff && read -r && git add . && git commit && git push ;;
-        *"up to date"*) echo "crypt is up to date, nothing left to do" ;;
+        *"behind"*) echo "repo is behind, pulling" && git pull ;;
+        *"ahead"*) echo "repo is ahead, pushing" && diff && read -r && git add . && git commit && git push ;;
+        *"up to date"*) echo "repo is up to date, nothing left to do" ;;
     esac
 }
